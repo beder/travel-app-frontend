11451a91f1acb31a8eba6b43b5439b6f13cc2863
Beder Acosta Borges <bedermail@yahoo.com>
2024-02-17 13:55:54 +0100

    Add Apollo client configuration and Tours page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "http://localhost:4000/graphql",
+      },
+    },
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/apollo", "@nuxtjs/tailwindcss"],
 });
---
+++ pages/Tours.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>There {{ !!data ? 'is' : 'is no' }} data.</p>
+  <p>There are {{ data?.tours?.length || 0 }} tours.</p>
+  <ul>
+    <li v-for="tour in data?.tours" :key="tour.id">
+      <p>{{ tour.name }}</p>
+      <p>{{ tour.startingDate }}</p>
+      <p>{{ tour.endingDate }}</p>
+      <p>{{ tour.price }}</p>
+    </li>
+  </ul>
+</template>
+<script lang="ts" setup>
+const query = gql`
+  query($travelSlug: String!, $priceFrom: Int, $priceTo: Int, $startingDate: Timestamp, $endingDate: Timestamp, $priceSortOrder: SortOrder, $skip: Int, $take: Int) {
+    tours(findToursInput: { travelSlug: $travelSlug, priceFrom: $priceFrom, priceTo: $priceTo, startingDate: $startingDate, endingDate: $endingDate, priceSortOrder: $priceSortOrder, skip: $skip, take: $take }) {
+      id
+      name
+      startingDate
+      endingDate
+      price
+    }
+  }
+`
+const variables = { travelSlug: "jordan-360" }
+const { data } = await useAsyncQuery(query, variables)
+</script>
