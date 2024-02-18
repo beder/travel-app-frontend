49142de34b9430a48ec4343fb91dedc00e621d83
Beder Acosta Borges <bedermail@yahoo.com>
2024-02-18 13:07:42 +0100

    Add Travels page

---
+++ pages/travels/index.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+const state = reactive({
+  skip: 0,
+  take: 5,
+})
+
+const { data } = await useAsyncGql('travels', state)
+</script>
+
+<template>
+  <div>
+    <h2>Travels</h2>
+
+    <ul>
+      <li
+        v-for="travel of data?.travels || []"
+        :key="travel.id"
+      >
+        {{ travel.name }} ({{ travel.description }})
+      </li>
+    </ul>
+
+    <button
+      :display="state.skip === 0"
+      :disabled="state.skip === 0"
+      @click="state.skip -= state.take"
+    >
+      Previous
+    </button>
+    <button
+      :disabled="data?.travels?.length < state.take"
+      @click="state.skip += state.take"
+    >
+      Next
+    </button>
+  </div>
+</template>
