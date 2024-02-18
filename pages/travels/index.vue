9d8496388a6153c774fd603b3db78f44258b0b4e
Beder Acosta Borges <bedermail@yahoo.com>
2024-02-18 17:42:24 +0100

    Add travel delete functionality

--- pages/travels/index.vue
+++ pages/travels/index.vue
@@ -1,10 +1,27 @@
 <script lang="ts" setup>
 const state = reactive({
-  skip: 0,
-  take: 5,
+  pagination: {
+    skip: 0,
+    take: 5,
+  },
+  modal: false,
+  deletionTargetSlug: "",
 });
 
-const { data } = await useAsyncGql("travels", state);
+const { data, refresh } = await useAsyncGql("travels", state.pagination);
+
+const confirmDeletion = async (slug: string) => {
+  state.modal = true;
+  state.deletionTargetSlug = slug;
+};
+
+const deleteTravel = async () => {
+  state.modal = false;
+
+  await useAsyncGql("deleteTravel", { slug: state.deletionTargetSlug });
+
+  refresh();
+};
 </script>
 
 <template>
@@ -21,25 +38,42 @@ const { data } = await useAsyncGql("travels", state);
         >
           Tours
         </UButton>
-        <span v-else> No tours available</span>
+        <span v-else>No tours available</span>
         <UButton :to="`/travels/${travel.slug}/tours/new`" target="_self">
-          New Tour
+          New Tours
         </UButton>
+        <UButton color="red" @click="confirmDeletion(travel.slug)"
+          >Delete Travel</UButton
+        >
       </li>
     </ul>
 
     <button
-      :display="state.skip === 0"
-      :disabled="state.skip === 0"
-      @click="state.skip -= state.take"
+      :display="state.pagination.skip === 0"
+      :disabled="state.pagination.skip === 0"
+      @click="state.pagination.skip -= state.pagination.take"
     >
       Previous
     </button>
     <button
-      :disabled="data?.travels?.length < state.take"
-      @click="state.skip += state.take"
+      :disabled="data?.travels?.length < state.pagination.take"
+      @click="state.pagination.skip += state.pagination.take"
     >
       Next
     </button>
+
+    <UModal v-model="state.modal" @close="state.modal = false">
+      <Card class="p-4 flex flex-col space-y-6">
+        <h2 class="text-xl text-primary font-semibold">Delete Travel</h2>
+
+        Are you sure you want to delete this travel?
+        <div class="flex items-start justify-end space-x-4">
+          <UButton color="red" class="px-4" @click="deleteTravel()"
+            >Yes</UButton
+          >
+          <UButton class="px-4" @click="state.modal = false">No</UButton>
+        </div>
+      </Card>
+    </UModal>
   </div>
 </template>
