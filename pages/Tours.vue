ca46cc5430e24c43b0bf9782d77eac7f85049333
Beder Acosta Borges <bedermail@yahoo.com>
2024-02-18 12:26:23 +0100

    Refactor Tours.vue component to display tour names and starting dates

--- pages/Tours.vue
+++ pages/Tours.vue
@@ -1,32 +1,18 @@
+<script lang="ts" setup>
+const { data } = await useAsyncGql('tours', { travelSlug: 'jordan-360' });
+</script>
+
 <template>
   <div>
-    <p>There {{ !!data ? 'is' : 'is no' }} data.</p>
-    <p>There are {{ data?.tours?.length || 0 }} tours.</p>
-    <ul>
-      <li
-        v-for="tour in data?.tours"
-        :key="tour.id"
-      >
-        <p>{{ tour.name }}</p>
-        <p>{{ tour.startingDate }}</p>
-        <p>{{ tour.endingDate }}</p>
-        <p>{{ tour.price }}</p>
-      </li>
-    </ul>
+    <h2>Tours</h2>
+
+    <p
+      v-for="(entry, i) of data?.tours"
+      :key="entry.id"
+    >
+      {{ i + 1 }}. Tour Name: {{ entry.name }} ({{
+        entry.startingDate
+      }})
+    </p>
   </div>
 </template>
-<script lang="ts" setup>
-const query = gql`
-  query($travelSlug: String!, $priceFrom: Int, $priceTo: Int, $startingDate: Timestamp, $endingDate: Timestamp, $priceSortOrder: SortOrder, $skip: Int, $take: Int) {
-    tours(findToursInput: { travelSlug: $travelSlug, priceFrom: $priceFrom, priceTo: $priceTo, startingDate: $startingDate, endingDate: $endingDate, priceSortOrder: $priceSortOrder, skip: $skip, take: $take }) {
-      id
-      name
-      startingDate
-      endingDate
-      price
-    }
-  }
-`
-const variables = { travelSlug: "jordan-360" }
-const { data } = await useAsyncQuery(query, variables)
-</script>
